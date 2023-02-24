0deb6f73420964e312870a6ab7fadece8f8487d2
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2023-02-24 15:35:59 +0000

    Initial Commit

---
+++ ExpenseTrackerApp/ExpenseTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerAppApp.swift
+//  ExpenseTrackerApp
+//
+//  Created by Sam on 24/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
